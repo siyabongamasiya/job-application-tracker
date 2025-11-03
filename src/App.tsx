e29a27f4fd9ad411debd92b3330df96e62fe609e
siyabonga <siyabongakhanyile76@gmail.com>
2025-11-03 14:40:46 +0200

    mod:added landing page to navigation

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,14 @@ import RegisterPage from "./pages/RegisterPage";
 import JobDetailsPage from "./pages/JobDetailsPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import { useEffect, useState } from "react";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/jobdetails/:jobId" element={<JobDetailsPage />} />
